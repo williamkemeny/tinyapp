0fcb12010f5b7934c1df873c9b3e57fa2c4ee701
William Kemeny <williamkemeny@gmail.com>
2023-05-26 15:23:35 +0000

    added a delete button

--- express_server.js
+++ express_server.js
@@ -61,6 +61,13 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.post("/urls/:id/delete", (req, res) => {
+  console.log(req.params.id);
+  delete urlDatabase[req.params.id];
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
 app.get("/u/:id", (req, res) => {
   const longURL = urlDatabase[req.params.id];
   res.redirect(longURL);
